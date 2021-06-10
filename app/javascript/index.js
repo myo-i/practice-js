2088a071931cd498b9933319e9d3f1dafd11f04e
myo-i <Imiyo0521@gmail.com>
2021-06-10 12:23:34 +0900

    テキストアニメーション実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,11 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const els = document.querySelector(".title");
-  const cb = function (entries, observe) {
-    entries.forEach((entry) => {
-      if (entry.isInteresting) {
-        const ta = new TextAnimation(entry, target);
-        ta.animate();
-      }
-    });
-  };
-});
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../index")
-require("../main")
+// require("../index")
+require("../text-animation")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
