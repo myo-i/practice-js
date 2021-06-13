8d61a5b0ae9b390f08ccce971841659fd612e495
myo-i <Imiyo0521@gmail.com>
2021-06-13 22:28:16 +0900

    スクロール監視とテキストアニメーション　実装

--- app/javascript/text-animation.js
+++ app/javascript/text-animation.js
@@ -40,3 +40,45 @@
 // };
 // const io = new IntersectionObserver(cb, options);
 // io.observe(child);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const cont = document.querySelector('.content');
+  const callback = function(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const ta = new TextAnimation(entry.target);
+        ta.animate();
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const options = {
+    root: null,
+    rootMarigin: "0px",
+    threshold: 0,
+  };
+
+  const io = new IntersectionObserver(callback, options);
+  els.forEach((el) => io.observe(el));
+})
+
+class TextAnimation {
+  constructor(el) {
+    this.DOM = {};
+    this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
+    this.chars = this.DOM.el.innerHTML.trim().split("");
+    this.DOM.el.innerHTML = this.splitText();
+  }
+
+  splitText() {
+    return this.chars.reduce((accu, curr) => {
+      curr = curr.replace(/\s+/, 'nbsp;');
+      accu = `${accu}<span class='char'>${curr}</span>`
+    },"");
+  }
+
+  animate() {
+    this.DOM.el.classList.toggle("inview");
+  }
+}
